4cea1d3c3af25f6b7b493ea5a1f7d61a3ab499dd
Paulo Vitor Bettini de Paiva Lima <paulovitorbal@gmail.com>
2024-01-08 11:58:28 -0300

    adicionando possiblidade de adicionar um novo registro

--- src/Controller/FolhaController.php
+++ src/Controller/FolhaController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\RegistroPonto;
+use App\Entity\TipoRegistro;
 use App\Repository\RegistroPontoRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,8 +21,22 @@ class FolhaController extends AbstractController
         ]);
     }
     #[Route('/folha/{ano}/{mes}', name: 'mostrar_folha')]
-    public function anoMes(RegistroPontoRepository $repository, int $ano, int $mes): Response
+    public function anoMes(RegistroPontoRepository $repository, int $ano, int $mes, Request $request, EntityManagerInterface $entityManager): Response
     {
+        if ($request->isMethod(Request::METHOD_POST)){
+
+            $registro = new RegistroPonto();
+            $registro->setDataRegistro(\DateTimeImmutable::createFromFormat('Y-m-d\\TH:i', $request->get('data')));
+            $registro->setTipo(TipoRegistro::INSERCAO);
+            $registro->setFuncionario(1);
+
+            $entityManager->persist($registro);
+            $entityManager->flush();
+            $this->addFlash(
+                'notice',
+                'Registro salvo!'
+            );
+        }
         return $this->render('folha/visualizar.html.twig', [
             'mes'=>$mes,
             'ano'=>$ano,
